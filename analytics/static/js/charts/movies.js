0e414af8f9950ac920a25f65e653ba37549213a4
Marko <markogajiin@gmail.com>
2020-09-12 21:07:02 +0200

    new layout

--- analytics/static/js/charts/movies.js
+++ analytics/static/js/charts/movies.js
@@ -1,18 +1,51 @@
 import { getChart } from './main.js'
 
-$(document).ready(() => {
-    const endpoint = '/charts/imdb?sorted=True'
+$(document).ready(() => init())
+
+function init() {
+    ajax('/charts/months', dateChart)
+    ajax('/charts/genres', genresChart)
+    ajax('/charts/imdb?sorted=True', topRatedChart)
+}
+
+function dateChart(response) {
+    const chart = getChart(
+        context('releasedChart'),
+        'verticalBar',
+        getData(response)
+    )
+}
+
+function genresChart(response) {
+    const chart = getChart(context('genresChart'), 'pie', getData(response))
+}
+
+const topRatedChart = (response) => {
+    const chart = getChart(
+        context('ratingsChart'),
+        'horizontalBar',
+        getData(response)
+    )
+}
+
+const ajax = (endpoint, func) => {
     $.ajax({
         method: 'GET',
         url: endpoint,
-        success: (response) => init(response),
-        error: (error) => {
-            alert('Error')
-        },
+        success: (response) => func(response),
+        error: (error) => alert(error.message),
     })
-})
+}
 
-const init = (response) => {
+const getData = (response) => {
+    return { data: response.data, labels: response.labels }
+}
+
+const context = (id) => {
+    return document.getElementById(id).getContext('2d')
+}
+
+const initChart = (response) => {
     const { data, labels } = response
     const chartData = { data, labels }
     const ctx = document.getElementById('ratingsChart').getContext('2d')
